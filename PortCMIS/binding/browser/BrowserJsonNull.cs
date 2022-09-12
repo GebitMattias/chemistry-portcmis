89f5dd2450b44891723ccfe857a28afacc1bb2ed
MattiasS <mattias.schoenke@gebit-ms.de>
2022-09-12 14:02:39 +0200

    Class to control the null value

---
+++ PortCMIS/binding/browser/BrowserJsonNull.cs
@@ -0,0 +1,12 @@
+﻿namespace PortCMIS.binding.browser
+{
+    /// <summary>The null value for Json</summary>
+    internal static class BrowserJsonNull
+    {
+        /// <summary>Default null string</summary>
+        private const string DefaultValue = "null";
+
+        /// <summary>Get the null value string</summary>
+        public static string Value { get; set; } = DefaultValue;
+    }
+}
