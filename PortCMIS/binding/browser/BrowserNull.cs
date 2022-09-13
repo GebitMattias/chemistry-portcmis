15a3c16442b3153dce02f71713deeaa16619dbce
MattiasS <mattias.schoenke@gebit-ms.de>
2022-09-13 14:03:24 +0200

    Null Value as constant

--- PortCMIS/binding/browser/BrowserNull.cs
+++ PortCMIS/binding/browser/BrowserNull.cs
@@ -3,10 +3,7 @@
     /// <summary>The null value for Binding</summary>
     internal static class BrowserNull
     {
-        /// <summary>Default null string</summary>
-        private const string DefaultValue = null;
-
-        /// <summary>Get the null value string</summary>
-        public static string Value { get; set; } = DefaultValue;
+        /// <summary>Value for NULL values</summary>
+        public const string Value = null;
     }
 }
